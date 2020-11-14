11a9b1f4d8f212a479ddbbc843ed5746747c773f
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2020-11-14 20:23:12 -0300

    insulating components remove styles from signIn and put in components

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -27,23 +27,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    button {
-      background: #ff9000;
-      height: 56px;
-      width: 100%;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      margin-top: 16px;
-      color: #312e38;
-      font-weight: 500;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
